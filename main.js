64699bb91c408dcfcdef13774ce93016b1223185
CyberPedro455 <62671231+CyberPedro455@users.noreply.github.com>
2022-06-30 15:14:00 -0700

    Finish hamburger menu

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 window.onload = () => {
+
+    //Hamburger click handler
     const menuBtn = document.querySelector('.hamburger');
-    const mobileNav = document.querySelector('.mobile-nav-ul')
+    const mobileNav = document.querySelector('.mobile-nav-ul');
     menuBtn.addEventListener('click', () => {
         menuBtn.classList.toggle('is-active');
         mobileNav.classList.toggle('is-active')
     })
+
+    
 }
