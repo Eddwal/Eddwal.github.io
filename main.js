54d558ab71ab795d4dc4bb4edab5207b31d311fb
CyberPedro455 <62671231+CyberPedro455@users.noreply.github.com>
2022-06-30 12:38:11 -0700

    Begin adding hamburger functionallity

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+    const menuBtn = document.querySelector('.hamburger');
+    const mobileNav = document.querySelector('.mobile-nav-ul')
+    menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle('is-active');
+        mobileNav.classList.toggle('is-active')
+    })
+}
